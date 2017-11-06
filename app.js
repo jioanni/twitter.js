8b0339d02f3c02a74c1559970ed9645d1b0d9086
Jason Ioannides <jason.ioannides@gmail.com>
2017-11-06 18:25:02 -0500

    server functional

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, function() {
+  console.log("Listening on 3000.")  
+})
+
+app.get('/', (req, res) => {
+    res.send("It is supes working.");
+})
