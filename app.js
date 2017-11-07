5c86c8eea2ba2b60353c14bbc50a0bdd78578918
Josh <jbaruch77@yahoo.com>
2017-11-07 11:48:30 -0500

    nunjucks mostly functional

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ const locals = {
   people: [
   {name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}
   ]
-}
+};
 
-nunjucks.configure('./views');
-nunjucks.render('index.html', locals, function(err, output) {
-  if (err) throw err;
-  console.log(output);
-});
+//nunjucks.configure('views');
+// nunjucks.render('index.html', locals, function(err, output) {
+  //   if (err) throw err;
+  //   console.log(output);
+  // });
+  app.set('view engine', 'html'); // have res.render work with html files
+  app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+  nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  app.use(volleyball);
+  app.listen(3000, function() {
+    console.log("Listening on 3000.")
+  })
 
-app.use(volleyball);
+  app.get('/:uri', (req, res) => {
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+    //res.render(console.log(req.method + " / " +req.params.uri));
 
-app.listen(3000, function() {
-  console.log("Listening on 3000.")
 })
-
-// app.get('/:uri', (req, res) => {
-//     res.send(console.log(req.method + " / " +req.params.uri));
-
-// })
